58be0b089c522223fc772e5df8b7c24aaaff09d0
Kilyan Slowinski <kilyan.slowinski@etudiant.univ-reims.fr>
2023-10-26 01:37:38 +0200

    validation.py in progress

---
+++ validation.py
@@ -0,0 +1,47 @@
+#validation.py
+import maillage 
+import math
+
+
+
+def u (x,n,y):
+    return 1.0+math.sin(0.5*math.pi*x) + n * (x - 4.0) * math.cos (0.5 * math.pi * y)
+
+def f():
+    return
+
+if __name__ == '__main__':
+    path_maillage = "Maillage/m4.msh"
+
+    [nbn,nbe,nba,coord,tri,ar,refn,reft,refa] = maillage.lit_fichier_msh(path_maillage)
+
+    codeDir = 1
+    uE = 1.0
+
+    fespace Vh(Th,P1); //Espace EF-P1
+    Vh uh, vh; //inconnue & Fct test
+    problem PBLAPLACE(uh, vh, solver=Cholesky) = 
+        int2d(Th)(dx(Uh)*dx(Vh)+dy(Uh)*dy(Vh)) 
+        - int2d(Th)(f*vh) 
+        + on (CodeDir, uh=uE);//décla = FV
+    PBLAPLACE;//Appel au solver EF => calcul de uh
+
+    //Calcul d'Erreurs : 
+    varf b(uh,vh) = int2d(Th)
+                    (dx(uh)*dx(vh)
+                    +dy(uh)*dy(vh)); //forme bilinéaire
+    matrix K = b(Vh,Vh); //matrice de rigidité 
+    Vh KEh, Eh = h-uh;
+    KEh[] = K * Eh[];
+    real eh = sqrt(Eh[]'*KEh[]); // ' == transposé 
+    cout << "eh = |u - uh|_H1 = " << eh << endl;
+
+    plot(Th,uh,cmm = "Solution EF-P1 uh");
+
+    //quand on voit le graphique ? -> l'aide
+    //                           f -> niveau de couleur / chaleur 
+    //                           N -> augmenter le niveau de couleur (la précision)w 
+    //                           m -> Montrer ou cacher le maillage 
+    //                           3 -> Passer en 3D 
+    //                           v -> 
+    // sequence f m v 3 N 
